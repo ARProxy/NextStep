b7d31f670676a5c5f4b2e1d45babc59cc951c9b1
ARProxy <hocci0222@kakao.com>
2024-06-25 23:36:52 +0900

    🚑️ Get Test
    sample test

--- Donghuii/src/test/java/webserver/RequestHandlerTest.java
+++ Donghuii/src/test/java/webserver/RequestHandlerTest.java
@@ -2,8 +2,31 @@ package webserver;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class RequestHandlerTest {
 	private static String testDirectory = "src/test/resources/";
 
-	
+	@Test
+	public void request_GET() throws Exception {
+		InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		Socket mockSocket = Mockito.mock(Socket.class);
+		Mockito.when(mockSocket.getInputStream()).thenReturn(in);
+		Mockito.when(mockSocket.getOutputStream()).thenReturn(out);
+
+		RequestHandler requestHandler = new RequestHandler(mockSocket);
+		requestHandler.run();
+
+		String response = out.toString();
+		assertTrue(response.contains("HTTP/1.1 200 OK"));
+	}
 }
